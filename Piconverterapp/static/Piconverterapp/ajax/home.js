0b784e07508a326006e299373dc5139870e2a1f9
uzomamicheal <uzomamicheal062@gmail.com>
2024-09-25 00:04:27 -0700

    created the menu navagation via screen size

--- Piconverterapp/static/Piconverterapp/ajax/home.js
+++ Piconverterapp/static/Piconverterapp/ajax/home.js
@@ -19,3 +19,26 @@ window.addEventListener('load', function () {
 okay_btn.onclick = function() {
     body.style.display = 'none';
 }
+
+// menu program
+
+var menu_btn = document.getElementById('menu-btn');
+var menu_body = document.getElementById('menu-show');
+var is_show = false
+
+menu_btn.onclick = function () {
+    if (is_show == false) {
+        menu_body.style.display = 'flex'
+        menu_body.style.scale = '1'
+        is_show = true
+    }else{
+        menu_body.style.display = 'none'
+        menu_body.style.scale = '0.8'
+        is_show = false
+    }
+}
+window.onresize = function () {
+    menu_body.style.display = 'none'
+    menu_body.style.scale = '0.8'
+    is_show = false
+}
