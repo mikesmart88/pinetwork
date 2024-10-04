7830c160ae08f7b215678c275e55986bbc3f196d
uzomamicheal <uzomamicheal062@gmail.com>
2024-10-04 16:58:44 -0700

    second section

--- Piconverterapp/static/Piconverterapp/ajax/home.js
+++ Piconverterapp/static/Piconverterapp/ajax/home.js
@@ -30,10 +30,15 @@ menu_btn.onclick = function () {
     if (is_show == false) {
         menu_body.style.display = 'flex'
         menu_body.style.scale = '1'
+        menu_btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">' +
+  '<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>' +
+'</svg>'
         is_show = true
     }else{
         menu_body.style.display = 'none'
         menu_body.style.scale = '0.8'
+        menu_btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-filter-left" viewBox="0 0 16 16">' +
+                '<path d="M2 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/>' + '</svg>'
         is_show = false
     }
 }
@@ -42,3 +47,31 @@ window.onresize = function () {
     menu_body.style.scale = '0.8'
     is_show = false
 }
+
+// observer program
+
+var nav = document.getElementById('navagation');
+var sect2 = document.getElementById('sect2');
+
+const observer = new IntersectionObserver(
+    callbackfunction,
+    option,
+)
+
+function callbackfunction(entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            nav.style.backgroundColor = 'rgb(109, 74, 180)';
+        }
+        else{
+            nav.style.backgroundColor = 'transparent'
+        }
+    });
+}
+
+var option = {
+    rootMargin: "0px",
+    threshold: 0.2,
+}
+
+observer.observe(sect2);
